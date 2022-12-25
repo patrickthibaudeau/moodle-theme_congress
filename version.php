a18ba26474bd2aa55dd2b899c8e3947baaaf9e83
Patrick Thibaudeau <thibaud@yorku.ca>
2022-12-25 18:21:09 -0500

    Added DataTable to header and footer.

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = '2022120401';
+$plugin->version = '2022122500';
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = '2022041900';
